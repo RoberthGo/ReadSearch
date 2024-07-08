031ca488855fa12d69573b7bb99675cd46831dbd
RoberthGo <robeg7183@gmail.com>
2024-07-08 16:59:35 -0600

    repository creation

---
+++ src/main/java/com/personal/project/ReadSearch/ReadSearchApplication.java
@@ -0,0 +1,13 @@
+package com.personal.project.ReadSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/project/ReadSearch/ReadSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.project.ReadSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
